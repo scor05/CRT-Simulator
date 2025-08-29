b891920abfc4fdd8b615d3b2950fa5f527519c8b
Santiago Cordero <santicordero28@gmail.com>
2025-08-29 08:57:29 -0600

    Esqueletos de main.py y electron.py creados, sprites agregadas.

--- src/Electron.py
+++ src/Electron.py
@@ -2,17 +2,37 @@
 Archivo de la clase de electron para guardar momentum
 """
 
+import os
 import math
+import pygame
 
-class Electron:
-    velocity = [] # componentes x,y
-    accel = [] # componentes x,y
-    pos = [] # componentes x,y
+class Electron(pygame.sprite.Sprite):
     mass = 9.11e-31
     
-    def __init__(self, Vox, Voy):
-        velocity = [Vox, Voy]
+    def __init__(self, Xo, Yo, VoX, VoY):
+        super().__init__()
+        self.pos = [Xo, Yo]
+        self.velocity = [VoX, VoY]
+        self.accel = [0,0]
+        
+        imagePath = os.path.join(os.path.dirname(__file__), "..", "res", "electron.png")
+        self.img = pygame.image.load(imagePath).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (Xo, Yo)
+        
         
     def applyForce(self, Fx: float, Fy: float) -> float:
-        accel = [Fx/self.mass, Fy/self.mass]
+        self.accel = [Fx/self.mass, Fy/self.mass]
+    
+    def update(self, dt: float): # deltaTime
+        """ 
+        dt -> intervalo de tiempo entre frames en seg.
+        """
         
+        self.velocity[0] += self.accel[0]*dt
+        self.velocity[1] += self.accel[1]*dt
+        
+        self.pos[0] += self.velocity[0]*dt
+        self.pos[1] += self.velocity[1]*dt
+        
+        self.rect.center = (self.pos[0], self.pos[1])
--- src/main.py
+++ src/main.py
@@ -3,6 +3,41 @@ Programa que corre la simulación del CRT
 """
 
 import math
+import sys
 import pygame
+import Electron
 
-k = 8.99e9 # Constante de coulomb
+K = 8.99e9 # Constante de coulomb
+e = 1.602e-19 # Carga fundamental
+FPS = 15
+gameClock = pygame.time.Clock()
+
+sprites = pygame.sprite.Group()
+
+def createWindow():
+    pygame.init()
+    screen = pygame.display.set_mode((1600,900))
+    pygame.display.set_caption("Simulador de CRT")
+    screen.fill((255,255,255))
+    gameLoop()
+
+def gameLoop():
+    running = True
+    while running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        dt = gameClock.tick(FPS) / 1000
+        
+        sprites.update(dt)
+        
+        
+        pygame.display.flip()
+    
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    createWindow()
